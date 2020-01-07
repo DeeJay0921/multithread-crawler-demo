7238fa6fb9b53f0d887e7128b25f30f916206c8d
43447 <Axios1996>
2020-01-07 09:14:59 +0800

    # 修改测试文件路径
    修复了错误的测试文件处理路径

---
+++ src/test/java/com/github/DeeJay0921/SmokeTest.java
@@ -0,0 +1,10 @@
+package com.github.DeeJay0921;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void test() {
+        System.out.println("This is Smoke Test");
+    }
+}
